f4035dc4d6a4dac5a6bf560193a39b367010e4b3
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-01-07 04:36:59 -0800

    made the code more self-documented

--- using_arrays/matrix_multiply.go
+++ using_arrays/matrix_multiply.go
@@ -2,8 +2,9 @@ package using_arrays
 
 import "fmt"
 
+// given two 1-D matrices, return their dot product
 func dot(a, b []int) int {
-	var result = 0
+	result := 0
 	for i := 0; i < len(a); i++ {
 		result += a[i] * b[i]
 	}
@@ -14,26 +15,26 @@ func dot(a, b []int) int {
 func mat_mul(mat1, mat2 [][]int) [][]int {
 	rows := len(mat1)
 	cols := len(mat2[0])
+	// initialize the result and pad with zeros
 	matrix := make([][]int, rows)
 	for i := range matrix {
-		matrix[i] = make([]int, cols)
 		// Each element in matrix[i] is automatically initialized to 0
+		matrix[i] = make([]int, cols)
 	}
-	fmt.Println(matrix)
 
+	// the resulting matrix should have the rows of matrix1 and the columns of matrix 2
 	for i := 0; i < rows; i++ {
 		for j := 0; j < cols; j++ {
 			// we havee to get the i_th row of A and the j_th column of B
-			curr_row := mat1[i]
-			fmt.Printf("Current row is: %v\n", curr_row)
-			curr_column := []int{}
+			row_matrix := mat1[i]
+			column_matrix := []int{}
+			// iterate through each row in matrix2 and get the jth element
+			// this will give us our desired column matrix
 			for _, item := range mat2 {
-				curr_column = append(curr_column, item[j])
+				column_matrix = append(column_matrix, item[j])
 			}
-			fmt.Printf("Curr col is %v\n", curr_column)
-
-			// calculate dot product
-			dot := dot(curr_row, curr_column)
+			// calculate dot product of both matrices
+			dot := dot(row_matrix, column_matrix)
 			matrix[i][j] = dot
 		}
 	}
@@ -42,8 +43,8 @@ func mat_mul(mat1, mat2 [][]int) [][]int {
 
 func Run() {
 	a := [][]int{
-		{1, 2},
-		{3, 4},
+		{7, 6},
+		{5, 9},
 	}
 	b := [][]int{
 		{5, 6},
